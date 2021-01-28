448d3a51f34273655b20c9da7091fa2d5170aaef
MarcoMereu <mereumarco@ymail.com>
2021-01-27 20:32:08 -0600

    Add files via upload

---
+++ fizzBuzz.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+#for num in range(1, 51):
+if num % 3 == 0 and num % 5 == 0:
+    print('FizzBuzz')
+elif num % 3 == 0:
+    print('Fizz')
+elif num % 5 == 0:
+    print('Buzz')
+else:
+    print("Number does not pass test:", num)
